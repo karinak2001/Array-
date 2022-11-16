da2d9fe583d514bb776e81ca7fb98b0f517874a0
Karina Kiryanov <karinak2001@edu.aac.ac.il>
2022-11-16 16:41:17 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        printArray(array);
+
+
+        public static double printArray (int[] array) { //function 1
+            for (int i=0; i<array.length; i++) {
+                System.out.println(array[i]);
+            }
+
+        }
+
+        public static double searchArray (int[] array,int number) {
+            for (int i=0; i<array.length; i++) {
+                if (number == array[i]) {
+                    System.out.println("this number is in the array");
+                }
+            }
+        }
+
+        public static double evenIndex (int[] array){
+            for (int i=0; i<array.length; i=i+2)
+                System.out.println(array[i]);
+        }
+
+
+        public static double evenNumber (int[] array){
+            for (int i=0; i<array.length; i++){
+                if ()
+            }
+        }
+
+
+
+
+    }
+}
